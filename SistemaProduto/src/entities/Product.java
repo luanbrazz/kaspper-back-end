3df6077aaa8c58686945520e4ba056d32f692f1e
Luan Braz <luan_silvacpv@hotmail.com>
2022-05-23 20:30:00 -0300

    aula: 23 de maio de 2022

--- SistemaProduto/src/entities/Product.java
+++ SistemaProduto/src/entities/Product.java
@@ -2,9 +2,31 @@ package entities;
 
 public class Product {
 	
-	public String name;
-	public double price;
-	public int quantity;
+	private String name;
+	private double price;
+	private int quantity;
+	
+	public Product() {
+	}
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
 	
 	public double totalValueInStock() {
 		return price * quantity;
