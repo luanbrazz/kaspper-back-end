7fd1dbc4efd8eb4959b489356fc87a232eeb35ba
Luan Braz <luan_silvacpv@hotmail.com>
2022-05-25 22:15:05 -0300

    aula: 25 de maio de 2022

--- SistemaProduto/src/application/Program.java
+++ SistemaProduto/src/application/Program.java
@@ -11,7 +11,7 @@ public class Program {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-			
+		
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
 		String name = sc.nextLine();
@@ -19,9 +19,12 @@ public class Program {
 		double price = sc.nextDouble();
 		
 		Product product = new Product(name, price);
-		
-		product.setName("Computer"); 
+				
+		product.setName("Computer");   
 		System.out.println("Updated name: " + product.getName());
+		
+		product.setPrice(1200.00);
+		System.out.println("Updated price: " + product.getPrice());
 		
 		System.out.println();
 		System.out.println("Product data: " + product);
--- SistemaProduto/src/entities/Product.java
+++ SistemaProduto/src/entities/Product.java
@@ -2,13 +2,14 @@ package entities;
 
 public class Product {
 	
-	private String name;
+	String name;
 	private double price;
 	private int quantity;
 	
+
 	public Product() {
 	}
-	
+
 	public Product(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
@@ -19,21 +20,33 @@ public class Product {
 		this.name = name;
 		this.price = price;
 	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
 	public double totalValueInStock() {
 		return price * quantity;
 	}
 	
 	public void addProducts(int quantity) {
-		this.quantity += quantity;
+		 this.quantity += quantity; 
 	}
 	
 	public void removeProducts(int quantity) {
@@ -41,13 +54,13 @@ public class Product {
 	}
 	
 	public String toString() {
-		return name 
-			+  ", $ "
-			+  String.format("%.2f", price)
-			+  ", "
-			+  quantity
-			+  " units, Total: $ "
-			+  String.format("%.2f", totalValueInStock());
+		return name
+			+ ", $ "
+			+ String.format("%.2f", price)
+			+ ", "
+			+ quantity
+			+ " units, Total: $ "
+			+ String.format("%.2f", totalValueInStock());
 	}
 
 }
