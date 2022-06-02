953052b70afd43bd12aa901890e2f8ec8cb914b3
Luan Braz <luan_silvacpv@hotmail.com>
2022-06-01 22:04:33 -0300

    aula: 01 de junho de 2022

---
+++ BoxingUnboxing/src/Application/Programa.java
@@ -0,0 +1,18 @@
+package Application;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		int x = 20;
+		
+		Integer obj = x;
+		
+		System.out.println(obj);
+		
+		int y = obj * 2;
+		
+		System.out.println(y);
+	}
+
+}
